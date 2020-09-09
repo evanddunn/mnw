13905cf5e70593ce6c465c33af49ce16c768f917
evan <evanddunn@gmail.com>
2020-09-09 17:37:31 -0400

    postbuild fix

--- express/app.js
+++ express/app.js
@@ -29,13 +29,13 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use(express.json())
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-  next();
-});
+// app.use(express.json())
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+//   next();
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
